e25eb731b20e30f7e92d8bcbfd0082fcb7da3300
Muhammed Fadera <muhammedfadera1@gmail.com>
2022-02-12 15:58:53 +0000

    solved

--- python/problem-30.py
+++ python/problem-30.py
@@ -1,5 +1,3 @@
-
-
 # Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
 #
 #     1634 = 1^4 + 6^4 + 3^4 + 4^4
@@ -12,16 +10,55 @@
 #
 # Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
 
-import numpy as np 
-import math as m
 
-digits = np.arange(10)
-digits5 = digits**5
+# Idea: find the largest n for which no number of that number of digits
+# can be equal to the sum of the digits.
+# use brute force from here but first sum the fifth power of the numbers taking
+# 2, 3, 4, ..., n at a time. Check if the numbers involved all appear in the result.
+# if so this number can be written as sum of the 5th power of its digit
 
-def choose(n, r):
-    # returns nCr
+p = range(10)
+g = [(str(i), i**5) for i in p]
 
-    return(
-        m.factorial(n)//(m.factorial(n-r)*m.factorial(r))
-    )
+def InStringRemove(digits, fifth_sum):
+    '''
+    checks if characters in chars are in string and remove the first instance of its appearance.
+    Otherwise it just return the string unaltered
+    '''
+    fifth_sum = str(fifth_sum)
+    l = len(fifth_sum)
+    for char in digits:
+        fifth_sum = fifth_sum.replace(str(char), '', 1)
+        if l == len(fifth_sum):
+            return False
+        l = len(fifth_sum)
+    if fifth_sum == '':
+        return True
 
+def ConcatDigits(num1, num2):
+    two_digits = []
+    for i in range(len(num1)):
+        for k in range(i, len(num2)):
+            k1, v1 = num1[i]
+            k2, v2 = num2[k]
+            two_digits.append((k1 + k2, v1 + v2))
+    return two_digits
+
+two_digits = ConcatDigits(g, g); two_digits
+three_digits = ConcatDigits(g, two_digits); three_digits
+four_digits = ConcatDigits(g, three_digits)
+five_digits = ConcatDigits(g, four_digits)
+six_digits = ConcatDigits(g, five_digits)
+# seven_digits = ConcatDigits(g, six_digits)
+# seven_digits = ConcatDigits(g, six_digits)
+all_nums = two_digits + three_digits + four_digits + \
+    five_digits + six_digits
+
+res = []
+for num in all_nums:
+    if num[1] not in res and InStringRemove(num[0], num[1]):
+        res.append(num[1])
+
+print('The sum of all numbers expressible as the sum' + \
+    f'of the fifth power of its digits is {sum(res)}')
+res
