e0cdb88792aceba924cad5f835e7e0370d412588
Fadera <mf572@exeter.ac.uk>
2025-06-08 21:29:33 +0100

    changes made to problem 37

--- python/problem-37.py
+++ python/problem-37.py
@@ -32,7 +32,13 @@ def isPrime(x):
    if (x <= 1) or (x % 2 == 0) or \
       (x %3 == 0) or (x % 5 == 0) or (x % 7 == 0):
       return False
-   for div in range(11, ceil(sqrt(x))+1):
+#    for div in prime_list:
+#       if x % div == 0:
+#          return False
+   min_prime = 11 
+#    if len(prime_list) > 0:
+#        min_prime = prime_list[-1]
+   for div in range(min_prime, ceil(sqrt(x))+1):
       if x % div == 0:
          return False
    return True
@@ -42,9 +48,9 @@ def cycle_one_digit_primes(x):
         return '2'
     elif x <= '2':
         return '3'
-    elif x <= '3':
+    elif x <= '4':
         return '5'
-    elif x <= '5':
+    elif x <= '6':
         return '7'
     else:
         return x
@@ -52,10 +58,12 @@ def cycle_one_digit_primes(x):
 def next_truncatable_number(x):
     x0 = x[0]
     if x0 in ['1', '4', '6']:
-        x0 = cycle_one_digit_primes(x0) + '0'*(len(x)-1)
-    elif x0 in ['8', '9']:
-        return str(int(x0) + 1) + '0'*(len(x) - 1)
+        x = cycle_one_digit_primes(x0) + '0'*(len(x)-1)
+    if x0 in ['8', '9']:
+        x = str(int(x0) + 1) + '0'*(len(x) - 1)
 
+    # first find the next number whose last digit is 
+    # 3, 5, 7
     last_digit = int(x[-1])
     if 7 <= last_digit <= 9: # moving on to the next number
         # 10 - last_digits takes us to the next next multiple of 10
@@ -63,8 +71,15 @@ def next_truncatable_number(x):
         x = str(int(x) + 10 - last_digit + 3)
     else:
         x = x[:-1] + cycle_one_digit_primes(str(last_digit))
-    if x[-1] in ['0', '1', '2', '4', '5', '6', '8', '9']:
-        x = next_truncatable_number(x)
+    # refine the next number 
+    last_digit = x[-1]
+    while last_digit in ['0', '1', '2', '4', '5', '6', '8', '9']:
+        if last_digit in ['0', '1', '2', '4', '5', '6']:
+            x = x[:-1] + cycle_one_digit_primes(last_digit)
+        elif last_digit in ['7', '8', '9']:
+            x = str(int(x) + 10 - int(last_digit) + 3)
+        last_digit = x[-1]
+        # x = next_truncatable_number(x) 
     return x
 
 
@@ -74,15 +89,16 @@ str_NUM = '11'
 result = 0
 prime_list = set()
 truncatable_prime_list = set()
-not_first_and_last_digits = [1, 4, 6, 8, 9]
+no_of_checks = 1
 while count < NUM_TRUNCATABLE_PRIMES:
     # if str_NUM[0] in not_first_and_last_digits and \
     #     str_NUM[-1] in not_first_and_last_digits:
     #     NUM += 1
     #     continue
     str_NUM = next_truncatable_number(str_NUM)
-    # print(f'checking the number {str_NUM}')
+    print(f'checking the number {str_NUM}')
     NUM = int(str_NUM)
+    no_of_checks += 1
     if NUM in truncatable_prime_list:
         result += NUM
         count += 1
@@ -92,6 +108,7 @@ while count < NUM_TRUNCATABLE_PRIMES:
     
     prime_cond = NUM in prime_list
     if not prime_cond:
+        # prime_cond = isPrime(NUM, prime_list=list(prime_list))
         prime_cond = isPrime(NUM)
         if prime_cond:
             prime_list.add(NUM)
@@ -106,7 +123,13 @@ while count < NUM_TRUNCATABLE_PRIMES:
     left_cond = True
     while left_cond and len(left_digit_removed) >= 1:
         # print(len(left_digit_removed))
+        if left_digit_removed[0] == '0':
+            left_digit_removed = left_digit_removed[1:]
+            continue
         int_left_digit_removed = int(left_digit_removed)
+        if int_left_digit_removed in truncatable_prime_list:
+            left_cond = True
+            break
         left_cond = int_left_digit_removed in prime_list
         if not left_cond:
             left_cond = isPrime(int_left_digit_removed)
@@ -118,6 +141,9 @@ while count < NUM_TRUNCATABLE_PRIMES:
     right_cond = True 
     while (left_cond and right_cond) and len(right_digit_removed) >= 1:
         int_right_digit_removed = int(right_digit_removed)
+        if int_right_digit_removed in truncatable_prime_list:
+            right_cond = True
+            break
         right_cond = int_right_digit_removed in prime_list
         if not right_cond:
             right_cond = isPrime(int_right_digit_removed)
@@ -132,5 +158,7 @@ while count < NUM_TRUNCATABLE_PRIMES:
         count += 1
         truncatable_prime_list.add(NUM)
         print(f"{count} truncable primes found")
+
+print(f"number of checks {no_of_checks}")
 print(f"The sum of all truncatble primes is {result}")
 # %%
