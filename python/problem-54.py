cda5e8dce9a0217566ec6e9d6289bf2b2c87f023
Fadera <mf572@exeter.ac.uk>
2025-06-26 11:56:44 +0100

    problem 54 solved!

--- python/problem-54.py
+++ python/problem-54.py
@@ -55,4 +55,156 @@ You can assume that all hands are valid (no invalid characters or repeated cards
 each player's hand is in no specific order, and in each hand there is a clear winner.
 
 How many hands does Player 1 win?
+
+
+
+# checking if the function works
+# hand1 = '5H 5C 6S 7S KD'
+# hand2 = '2C 3S 8S 8D TD'
+# hand1 = '5D 8C 9S JS AC'
+# hand2 = '2C 5C 7D 8S QH'
+# hand1 = '2D 9C AS AH AC'
+# hand2 = '3D 6D 7D TD QD'
+# hand1 = '4D 6S 9H QH QC'
+# hand2 = '3D 6D 7H QD QS'
+# hand1 = '2H 2D 4C 4D 4S'
+# hand2 = '3C 3D 3S 9S 9D'
+hand1 = '8C TS KC 9H 4S'
+hand2 = '7D 2S 5D 3S AC'
+# rank(hand1)
+winner(hand1, hand2)
+
 """
+#%%
+from time import time
+t1 = time()
+# 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, Ace.
+card_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6':6, '7': 7, '8': 8, '9': 9, 
+               'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+
+
+def suit_counter(hand):
+    output = dict()
+    for s in ['H', 'C', 'S', 'D']:
+        if s in hand:
+            output[s] = hand.count(s)
+    return output
+
+def is_hand_consecutive_valued(min_value, values):
+# def is_hand_consecutive_valued(hand):
+    for v in range(min_value, min_value+5):
+        if v not in values:
+            return False
+    return True
+    
+# ranking a hand
+def rank(hand):
+    S = suit_counter(hand)
+    l = len(S)
+    min_value = 100
+    values = []
+    value_counts = {}
+    card_value_hist = {}
+    for c in hand:
+        if c in card_values:
+            v = card_values[c]
+            try:
+                value_counts[v] += 1
+            except KeyError:
+                value_counts[v] = 1
+            values.append(v)
+            min_value = min(min_value, v)
+    for v, c in value_counts.items(): 
+        try:
+            card_value_hist[c].append(v)
+        except KeyError:
+            card_value_hist[c] = [v]
+
+    if 'T' in hand and 'J' in hand and 'Q' in hand and 'K' in hand and \
+        'A' in hand and l == 1:
+        return 10, card_value_hist
+    elif l == 1:
+        if is_hand_consecutive_valued(min_value, values):
+            return 9, card_value_hist
+        else:
+            return 6, card_value_hist
+    elif 4 in card_value_hist:
+        return 8, card_value_hist
+    elif 3 in card_value_hist:
+        if 2 in card_value_hist:
+            return 7, card_value_hist
+        else:
+            return 4, card_value_hist
+    elif is_hand_consecutive_valued(min_value, values):
+        return 5, card_value_hist
+    elif 2 in card_value_hist:
+        if len(card_value_hist[2]) >= 2:
+            return 3, card_value_hist
+        else:
+            return 2, card_value_hist
+    else:
+        return 1, card_value_hist
+    
+def winner(hand1, hand2):
+    r1, value_counts1 = rank(hand1)
+    r2, value_counts2 = rank(hand2)
+    if r1 > r2:
+        return 1
+    elif r1 < r2:
+        return 0
+    else:
+        c1 = list(value_counts1.keys())
+        c2 = list(value_counts2.keys())
+        while len(c1) > 0 and len(c2) > 0:
+            m1 = max(c1)
+            m2 = max(c2)
+            v1 = value_counts1[m1]
+            v2 = value_counts2[m2]
+            while len(v1) > 0 and len(v2) > 0:
+                m11 = max(v1)
+                m22 = max(v2)
+                if m11 > m22:
+                    return 1
+                elif m11 < m22:
+                    return 0
+                else:
+                    v1.remove(m11)
+                    v2.remove(m22)
+            else:
+                c1.remove(m1)
+                c2.remove(m2)
+        # print(f"We should never reach here")
+        return -1
+with open("0054_poker.txt", 'r') as f:
+    # game = f.readlines()
+    game = f.read()
+player1 = ''
+player2 = ''
+spaces = 0
+player1_winner_counter = 0
+for c in game:
+    # print(c)
+    if c == ' ':
+        spaces += 1
+        if spaces < 5:
+            player1 += c
+        elif spaces > 5:
+            player2 += c
+    elif c == '\n':
+        w = winner(player1, player2)
+        if w == -1:
+            print(player1)
+            print(player2)
+            break
+        else:
+            player1_winner_counter += winner(player1, player2)
+            player1 = ''
+            player2 = ''
+            spaces = 0
+    elif spaces < 5:
+        player1 += c
+    elif spaces >= 5:
+        player2 += c
+
+print(f"Player 1 won {player1_winner_counter} times")
+print(f"Time taken: {time() - t1: .2f} seconds")
