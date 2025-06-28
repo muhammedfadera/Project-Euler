2ca787c06d1dd15a29f106794151de7c3b840c3e
Fadera <mf572@exeter.ac.uk>
2025-06-28 20:07:21 +0100

    problem 58 and 60 solved!

---
+++ python/problem-58.py
@@ -0,0 +1,45 @@
+"""
+Starting with 1 and spiralling anticlockwise in the following way, a square spiral with side length 7 is formed:
+   {37}  36   35   34   33   32  {31}
+    38  {17}  16   15   14  {13}  30
+    39   18  {5}    4  {3}   12   29
+    40   19   6   {1}   2    11   28
+    41   20  {7}    8   9    10   27
+    42   21   22   23   24   25   26
+   {43}  44   45   46   47   48  {49}
+
+It is interesting to note that the odd squares (1, 9, 25, 49, ...) lie along the 
+bottom-right diagonal.
+
+More interestingly, 8 out of the 13 numbers lying along both diagonals are prime numbers, 
+which gives a prime ratio of 8 out of 13, or approximately 61.5%.
+
+When one complete new layer is wrapped around the spiral above, a new square 
+spiral with side length 9 is formed.
+
+If this process is continued, adding new layers to increase the side length by 2 
+each time (9, 11, 13, ...), at what side length does the ratio of primes along 
+both diagonals first fall below 10%?
+"""
+#%%
+from utilities import is_prime
+from time import time
+t1 = time()
+n_diagonal = 1 # the one at the start
+n_sides = 1
+prime_proportion = 1.0
+n_primes = 0
+while prime_proportion >= 0.1:
+    n_sides += 2
+    square_corner = n_sides**2
+    # for k in range(1, 4):
+    #     p = square_corner + k*(- n_sides+1)
+    #     if is_prime(p):
+    #         n_primes += 1
+    n_primes += sum([is_prime(square_corner + k*(- n_sides+1)) for k in range(1, 4)])
+    n_diagonal += 4
+    prime_proportion = n_primes/n_diagonal
+
+print("The side length at which the proportion of primes on",
+      f"the diagonal falls below 10 percent is {n_sides}")
+print(f"Time taken: {time() - t1: .2f} seconds")
---
+++ python/problem-60.py
@@ -0,0 +1,71 @@
+"""
+The primes 3, 7, 109, and 673 are quite remarkable. 
+By taking any two of these primes and concatenating them in any order, 
+the result is always a prime. For example, taking 7 and 109, both 7109 and 1097 are prime.
+
+The sum of these four primes, 792, represents the lowest sum for a set of four 
+primes with this property.
+
+Question:
+Find the lowest sum for a set of five primes for which any two primes 
+concatenate to produce another prime.
+
+forward and backward pass
+"""
+# from 
+#%%
+from utilities import is_prime
+from time import time
+t1 = time()
+def is_prime_from_list(x, prime_list_less_than_x):
+    sqrt_x = int(x**0.5)
+    for p in prime_list_less_than_x:
+        if p <= sqrt_x and x % p == 0:
+            return False
+    return True
+
+min_seq_length = 6
+primes_forward_pass = dict()
+prime_list_less_than_x = [2]
+prime_list = set([2])
+k = 3
+
+
+while True:
+    # if not is_multiple_of_3(k) and is_prime_from_list(k, prime_list_less_than_x):
+    if is_prime_from_list(k, prime_list_less_than_x):
+        for p in prime_list_less_than_x:
+            N = int("".join([str(p), str(k)]))
+            # if not is_multiple_of_3(N) and is_prime(N):
+            if is_prime(N):
+                prime_list.add(N)
+                N_rev = int("".join([str(k), str(p)]))
+                # if not is_multiple_of_3(N_rev) and is_prime(N_rev):
+                if is_prime(N_rev):
+                    prime_list.add(N_rev)
+                    try:
+                        primes_forward_pass[k].append(p)
+                    except KeyError:
+                        primes_forward_pass[k] = [p]
+        prime_list_less_than_x.append(k)
+    k += 1
+    try:
+        current_prime_list_k = primes_forward_pass[k-1]
+        if len(current_prime_list_k) >= min_seq_length:
+            test_prime = [current_prime_list_k[0]]
+            m = 1
+            while m < len(current_prime_list_k):
+                p_in_the_list = current_prime_list_k[m]
+                cond = sum([p in primes_forward_pass[p_in_the_list] for p in test_prime])
+                if cond == len(test_prime):
+                    test_prime.append(p_in_the_list)
+                m += 1
+            if len(test_prime) == min_seq_length - 1:
+                test_prime = test_prime + [k-1]
+                print(f"The required list of primes is {test_prime}") 
+                break
+    except KeyError:
+        continue
+
+print(f"Result is {sum(test_prime)}")
+print(f"Time taken: {time() - t1:.2f} seconds")
